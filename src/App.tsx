84dac0894a5917521ec66f6bc25b3a122a4ad7f8
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2025-06-11 15:43:21 +0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import "./App.css";
-import Section from "./components/Section";
 import Header from "./components/Header";
+import Section from "./components/Section";
 import AtomsCards from "./components/AtomCards";
 import TemplatesCards from "./components/TemplatesCards";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <>
+          <Header />
+          <Section name="Atoms" />
+          <AtomsCards />
+          <Section name="Templates" />
+          <TemplatesCards />
+        </>
+      ),
+    },
+  ],
+  {
+    basename: "/apple-pay", 
+  }
+);
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Section name={"Atoms"} />
-      <AtomsCards />
-      <Section name={"Templates"} />
-      <TemplatesCards/>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
